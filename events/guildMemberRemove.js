73d517405e2ccd73ec66c1e54e7d66a7fa3c938c
mohamedsabry0ss <56240404+mohamedsabry0ss@users.noreply.github.com>
2020-07-18 17:54:57 +0200

    Add files via upload

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,67 @@
+module.exports = async(fetchAuditLogs,member) => {
+  const entry1 = await member.guild
+    .fetchAuditLogs()
+    .then(audit => audit.entries.first());
+  if (entry1.action === "MEMBER_KICK") {
+    const entry2 = await member.guild
+      .fetchAuditLogs({
+        type: "MEMBER_KICK"
+      })
+      .then(audit => audit.entries.first());
+    const fs = require('fs');
+     let config = JSON.parse(fs.readFileSync("./Database/AntiHackConfig.json", "UTF8"));
+    const entry = entry2.executor;
+    if (!config[member.guild.id])
+      config[member.guild.id] = {
+        banLimit: 3,
+        chaDelLimit: 3,
+        chaCrLimit: 3,
+        roleDelLimit: 3,
+        kickLimits: 3,
+        roleCrLimits: 3,
+        time: 30
+      };
+    var anti = JSON.parse(fs.readFileSync("./Database/AntiHack.json", "UTF8"));
+    if (!anti[member.guild.id + entry.id]) {
+      anti[member.guild.id + entry.id] = {
+        actions: 1
+      };
+      setTimeout(() => {
+        anti[member.guild.id + entry.id].actions = 0;
+      }, config[member.guild.id].time * 1000);
+    } else {
+      anti[member.guild.id + entry.id].actions = Math.floor(
+        anti[member.guild.id + entry.id].actions + 1
+      );
+      console.log("TETS");
+      setTimeout(() => {
+        anti[member.guild.id + entry.id].actions = 0;
+      }, config[member.guild.id].time * 1000 || 30000);
+      if (
+        anti[member.guild.id + entry.id].actions >=
+        config[member.guild.id].kickLimits
+      ) {
+        member.guild.members.cache
+          .get(entry.id)
+          .ban()
+          .catch(e =>
+            member.owner.send(
+              `**⇏ | ${entry.username} حاول حظر جميع الأعضاء **`
+            )
+          );
+        anti[member.guild.id + entry.id].actions = 0;
+              fs.writeFile("./Database/AntiHackConfig", JSON.stringify(config, null, 2), function(
+        e
+      ) {
+        if (e) throw e;
+      });
+      fs.writeFile("./Database/AntiHack.json", JSON.stringify(anti, null, 2), function(
+        e
+      ) {
+        if (e) throw e;
+      });
+    }
+  }
+
+}
+}
---
+++ events/roleDelete.js
@@ -0,0 +1,67 @@
+module.exports = async (fetchAuditLogs,channel) => {
+  const entry1 = await channel.guild
+    .fetchAuditLogs({
+      type: "ROLE_DELETE"
+    })
+    .then(audit => audit.entries.first());
+  console.log(entry1.executor.username);
+  const entry = entry1.executor;
+
+  if (!config[channel.guild.id])
+    config[channel.guild.id] = {
+      banLimit: 3,
+      chaDelLimit: 3,
+      chaCrLimit: 3,
+      roleDelLimit: 3,
+      kickLimits: 3,
+      roleCrLimits: 3,
+      time: 30
+    };
+  var config = JSON.parse(fs.readFileSync("./Database/AntiHackConfig.json", "UTF8"));
+  const fs = require("fs");
+  var anti = JSON.parse(fs.readFileSync("./Database/AntiHack.json", "UTF8"));
+  if (!anti[channel.guild.id + entry.id]) {
+    anti[channel.guild.id + entry.id] = {
+      actions: 1
+    };
+    setTimeout(() => {
+      anti[channel.guild.id + entry.id].actions = "0";
+    }, config[channel.guild.id].time * 1000);
+  } else {
+    anti[channel.guild.id + entry.id].actions = Math.floor(
+      anti[channel.guild.id + entry.id].actions + 1
+    );
+    console.log("TETS");
+    setTimeout(() => {
+      anti[channel.guild.id + entry.id].actions = "0";
+    }, config[channel.guild.id].time * 1000);
+    if (
+      anti[channel.guild.id + entry.id].actions >=
+      config[channel.guild.id].roleDelLimit
+    ) {
+      channel.guild.members.cache
+        .get(entry.id)
+        .ban()
+        .catch(e =>
+          channel.guild.owner.send(
+            `**⇏ | ${entry.username} قام بمسح الكثير من الرتب **`
+          )
+        );
+      anti[channel.guild.id + entry.id].actions = "0";
+  fs.writeFile(
+    "./Database/AntiHackConfig.json",
+    JSON.stringify(config, null, 2),
+    function(e) {
+      if (e) throw e;
+    }
+  );
+  fs.writeFile(
+    "./Database/AntiHack.json",
+    JSON.stringify(anti, null, 2),
+    function(e) {
+      if (e) throw e;
+    }
+  );
+}
+}
+}
